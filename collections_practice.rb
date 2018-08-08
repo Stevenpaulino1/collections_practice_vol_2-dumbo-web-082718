664f4c4cec40b0d4ad4de2576c7412deaa76b944
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-08 22:02:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
-# your code goes here
+def begins_with_r(arr)
+  output = true
+  arr.each do | element |
+    result = element.start_with? 'r' 
+      if result == false 
+        return false 
+      end 
+  end
+  output
+end
+
+def contain_a(array)
+  array.select {|element| element.include? 'a'}
+end
+
+def first_wa(words)
+  words.find { |word| word.to_s.start_with?('wa') }
+end
+
+def remove_non_strings(array)
+  array.select {|element| element.is_a? String }
+end
+
+#def count_elements(array)
+#end
+
+#def merge_data
+#end
+
+# def find_cool
+#end
+
+#def organize_schools
+#end
