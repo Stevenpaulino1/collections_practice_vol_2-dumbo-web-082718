a89aee00a9a281e767d36f3fe74891b3ba75fe97
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-13 02:10:33 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,14 +21,42 @@ def remove_non_strings(array)
   array.select {|element| element.is_a? String }
 end
 
-#def count_elements(array)
-#end
+def count_elements(array)
+   new_hash = array.each do |element|
+    element[:count] = array.count(element)
+  end
+   new_hash.delete_at(1)
+   new_hash
+ end
 
-#def merge_data
-#end
+def merge_data(data_1, data_2)
+  merged = []
+   data_1.each do |item|
+     item.each do |k, name|
+      merged << item.merge(data_2[0][name])
+    end
+  end
+  merged.flatten
+end
 
-# def find_cool
-#end
+ 
+   def find_cool(array)
+  array.select do |item|
+    item.values.include?('cool')
+  end
+end
 
-#def organize_schools
-#end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
